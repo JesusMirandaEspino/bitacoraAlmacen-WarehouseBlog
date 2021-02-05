62f95595715769ba3f2534fb5d9c2da633ff356e
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-02-04 21:22:29 -0600

    Formulario de ruta
    Se crea el formulario inicial para ingresar la informacion de la ruta, asi como la base de como se mostrara

--- assets/php/pages/rutaspages/navrutas.php
+++ assets/php/pages/rutaspages/navrutas.php
@@ -4,6 +4,7 @@
                 <ul class="col-full-12">
                     <li><a href="../../pages/rutas.php">Rutas</a></li>
                     <li><a href="rutadia.php">Ingresar Ruta del dia</a></li>
+                    <li><a href="rutainfo.php">Ver ruta del dia</a></li>
                     <li><a href="rutasemana.php">Acumulado Semanal</a></li>
                     <li><a href="rutamensual.php">Acumlado Mensual</a></li>
                     <li><a href="">Historico</a></li>
--- assets/php/pages/rutaspages/rutadia.php
+++ assets/php/pages/rutaspages/rutadia.php
@@ -35,6 +35,176 @@
     <main>
 
 
+        <section class="fila">
+
+            <div class="contenedor1">
+
+                <div class="col-full-12">
+                    <h2>Ingresa informacion de la ruta del dia</h2>
+                </div>
+
+            </div>
+
+            <!--TODO Formulario entrada de informacion de la ruta del dia    -->
+            <form class="fila">
+
+                <!--  Primera parte del Formulario chofer y unidad -->
+                <div class="contenedor1">
+
+                    <div class="col-full-2">
+                        <label for="date">Fecha</label>
+                        <input type="date" name="date" id="date">
+                    </div>
+
+                    <div class="col-full-2">
+                        <label for="driver">Chofer</label>
+                        <select>
+                            <option value="driver1">Chofer1</option>
+                            <option value="driver2">Chofer2</option>
+                            <option value="driver3">Chofer3</option>
+                        </select>
+                    </div>
+
+                    <div class="col-full-2">
+                        <label for="copilot">Copiloto</label>
+                        <select>
+                            <option value="copilo1">Copiloto1</option>
+                            <option value="copilo2">Copiloto2</option>
+                            <option value="copilo3">Copiloto3</option>
+                        </select>
+                    </div>
+
+                    <div class="col-full-2">
+                        <label for="vehicle">Unidad</label>
+                        <select>
+                            <option value="vehicle1">Unidad1</option>
+                            <option value="vehicle2">Unidad2</option>
+                            <option value="vehicle3">Unidad3</option>
+                        </select>
+                    </div>
+
+                    <div class="col-full-2">
+                        <label for="kmentry">KM Salida</label>
+                        <input type="number" name="kmentry" id="kmentry">
+                    </div>
+
+                    <div class="col-full-2">
+                        <label for="kmarrival">KM Llegada</label>
+                        <input type="number" name="kmarrival" id="kmarrival">
+                    </div>
+
+                </div>
+                <!--  *********************************** -->
+
+
+                <!--  Segunda parte del Formulario Pedido -->
+                <div class="contenedor1">
+
+                    <div class="col-full-3">
+                        <label for="order">Tipo de Pedido</label>
+                        <select>
+                            <option value="order1">En Ruta</option>
+                            <option value="order2">Cliente Recoge</option>
+                        </select>
+                    </div>
+
+                    <div class="col-full-3">
+                        <!--  Esta la tendra que dar en automatico -->
+                        <label for="store">Tienda origen</label>
+                        <select>
+                            <option value="store1">Federacion</option>
+                            <option value="store2">Independiente</option>
+                        </select>
+                    </div>
+
+                    <div class="col-full-3">
+                        <label for="requested">$ Solicitado</label>
+                        <input type="number" name="requested" id="requested">
+                    </div>
+
+                    <div class="col-full-3">
+                        <label for="assortment">$ Surtido</label>
+                        <input type="number" name="assortment" id="assortment">
+                    </div>
+
+                </div>
+                <!--   ***********************************  -->
+
+
+                <!--  Tercera parte del Formulario Cliente -->
+                <div class="contenedor1">
+
+                    <div class="col-full-4">
+                        <!--  Esta la tendra que dar en automatico -->
+                        <label for="clientname">Nombre del cliente</label>
+                        <input type="text" name="clientname" id="clientname">
+                    </div>
+
+                    <div class="col-full-4">
+                        <!--  Esta la tendra que dar en automatico -->
+                        <label for="clientaddres">Domicilio</label>
+                        <input type="text" name="clientaddres" id="clientaddres">
+                    </div>
+
+                    <div class="col-full-4">
+                        <!--  Esta la tendra que dar en automatico -->
+                        <label for="clientcity">Minucipio</label>
+                        <input type="text" name="clientcity" id="clientcity">
+                    </div>
+
+                </div>
+                <!--   ***********************************  -->
+
+
+                <!--  Cuarta parte del Formulario Informacion adicional -->
+                <div class="contenedor1">
+
+                    <div class="col-full-4">
+                        <!--  Esta la tendra que dar en automatico -->
+                        <label for="attended">Recibio</label>
+                        <input type="text" name="attended" id="attended">
+                    </div>
+
+                    <div class="col-full-4">
+                        <!--  Esta la tendra que dar en automatico -->
+                        <label for="schedule">Horario</label>
+                        <input type="text" name="schedule" id="schedule">
+                    </div>
+
+                    <div class="col-full-4">
+                        <!--  Esta la tendra que dar en automatico -->
+                        <label for="incidence">Incidencia</label>
+                        <select>
+                            <option value="vehicle1">Ninguna</option>
+                            <option value="vehicle2">Faltante</option>
+                            <option value="vehicle3">Daño</option>
+                        </select>
+                    </div>
+
+                </div>
+                <!--   ***********************************  -->
+
+
+                <div class="contenedor1">
+
+                    <div class="col-full-12">
+
+                        <h4>Observaciones</h4>
+                        <textarea name="" id="" cols="30" rows="10">
+                        </textarea>
+
+                    </div>
+
+                </div>
+
+            </form>
+            <!--TODO Formulario entrada de informacion de la ruta del dia    -->
+
+
+
+        </section>
+
+
     </main>
 
 
