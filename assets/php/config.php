0479e2f4c4dc182ee3934194425d27ffd438e114
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-02-25 22:10:48 -0600

    Configuracion a base de datos
    Se comienza a configurar la base de datos.

--- assets/php/config.php
+++ assets/php/config.php
@@ -3,5 +3,18 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 define('SITE_URL', 'http://localhost/bitacora');
+define('SITE_TIMEZONE', ['es', 'spa', 'es_ES']);
+define('SITE_LANG', 'America/Mexico_City');
+
+
+// Conexion a base de datos
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'warehouseblog');
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+
+/*define('ADMIN_USER', 'username');
+define('ADMIN_PASS', 'userpassword');*/
 
 ?>
